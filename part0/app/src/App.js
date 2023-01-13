d2b1cab71d695c375792003e7fb16e42f741b096
Zachary Moseti <44673237+ZachyDev@users.noreply.github.com>
2023-01-13 19:41:46 +0000

    using useState() to manage component state

--- part0/app/src/App.js
+++ part0/app/src/App.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { useState } from 'react'
 
 function App() {
-  const [counter, setCounter] = useState(0)
+  const [counter,setCounter] = useState(0);
+  // update counter value with setTimeOut function
   setTimeout(() => {
-    setCounter(counter + 1)
-  },1000)
- 
+    setCounter(counter+1)
+  }, 1000)
   return (
-    <div className="App">
-      <p>Counter: {counter}</p>
+    <div>
+      <h4>{counter}</h4>
     </div>
   )
 }
